b6ab1027d48e20ef27da9f650b09319a9bfe3c26
Muhammad Moeez <moeezsalman246@gmail.com>
2026-03-04 02:17:42 +0500

    minor changes

--- priorify/src/App.jsx
+++ priorify/src/App.jsx
@@ -1,10 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./components/Signup";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/signup" replace />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
