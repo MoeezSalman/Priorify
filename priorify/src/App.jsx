919b97fee681d302b1bfdf33fc5e68c7fcfe1f8f
Muhammad Moeez <moeezsalman246@gmail.com>
2026-03-05 02:20:31 +0500

    reset password is added

--- priorify/src/App.jsx
+++ priorify/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import FeatureGraphs from "./components/Featuregraphs";
-
+import ForgotPassword from "./components/ForgotPassword";
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<FeatureGraphs />} />
+        <Route path="/forgot" element={<ForgotPassword />} />
       </Routes>
     </Router>
   );
