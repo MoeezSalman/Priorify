b52d3fb66c4c80d27a7fb9cd8a06462c361339b7
M.Mustafa <mm037925@gmail.com>
2026-03-06 01:22:14 +0500

    Dashboard page is about to complete

--- priorify/src/App.jsx
+++ priorify/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import FeatureGraphs from "./components/Featuregraphs";
+import Dashboard from "./components/Dashboard";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<FeatureGraphs />} />
+        <Route path="/Dashboard12" element={<Dashboard />} />
       </Routes>
     </Router>
   );
