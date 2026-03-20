beb084ae12e84cfe791020a315401b60f11293b7
Muhammad Moeez <moeezsalman246@gmail.com>
2026-03-21 00:33:21 +0500

    pdf is successfully sending

--- priorify/backend/server.js
+++ priorify/backend/server.js
@@ -8,7 +8,8 @@ const adminRoutes = require("./src/routes/admin.routes");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use("/api/admin", adminRoutes);
 app.use("/api/feedback", feedbackRoutes);
 
